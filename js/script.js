dd20a39d30873c1a9cbcce936de865cbd05fe1aa
Cypher <rafatrias.27@gmail.com>
2025-04-25 16:22:04 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const startButton = document.getElementById("startButton");
+const time = document.getElementById("time");
+const video = document.getElementById("video");
+let countdownValue = 59;
+let interval;
+
+const countdown = () => {
+    startButton.classList.add("hidden");
+    interval = setInterval(updateCountdown, 1000);
+};
+
+const updateCountdown = () => {
+    if (countdownValue > 0) {
+        time.textContent = `${countdownValue} sec`;
+        countdownValue--;
+    } else {
+        clearInterval(interval);
+        video.play();
+        time.textContent = "¡Despegue!";
+        countdownValue = 59;
+        setTimeout(() => {
+            startButton.classList.remove("hidden");
+            time.textContent = "60 sec";
+        }, 16000)
+    }
+};
+
+startButton.addEventListener("click", countdown);
